ed725ca792159a74b1e555ce3db176fe6c025d30
Desislava Naydenova <deskata.01@gmail.com>
2024-04-24 11:39:59 +0000

    Add code for subtract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,6 +70,10 @@ function calculateCorrecrAnswer() {
         return [operand1 + operand2, "addition"];
     } else if(operator === "X"){
         return [operand1 * operand2, "multiply"];
+    }else if(operator === "-"){
+        return [operand1 - operand2, "subtract"];
+    }else if(operator === "/"){
+        return [operand1 / operand2, "division"];
     }else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}! Aborting!`;
@@ -100,11 +104,17 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 
 }
 
-function displayDivideQuestion() {
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 
 }
 
