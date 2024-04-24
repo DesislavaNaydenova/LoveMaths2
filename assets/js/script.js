b95800851526e9815e3ea5640498cdd7b2b43ef2
Desislava Naydenova <deskata.01@gmail.com>
2024-04-24 09:40:55 +0000

    Add functions to the Buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Wait for the DOM  to finish loading befor starting the game
+// Getthe button elements and add the event listeners to them
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            }else {
+                let dataType = this.getAttribute("data-type");
+                alert(`You clicked ${dataType}!`);
+            }
+
+        })
+    }
+})
+
 function runGame() {
 
 }
