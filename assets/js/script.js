ba613545e1ca4c1eeacae16d2ead5e218c5fd26c
Desislava Naydenova <deskata.01@gmail.com>
2024-04-24 10:14:18 +0000

    Diplsy an addition querstion to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,30 @@ document.addEventListener("DOMContentLoaded", function(){
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit");
             }else {
-                let dataType = this.getAttribute("data-type");
-                alert(`You clicked ${dataType}!`);
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
             }
 
         })
     }
+    runGame("addition")
 })
 /**
  * The main game Loop, called when the script is first loaded
  * and after the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    }else {
+        alert(`Unknown gametype: ${gameType}`);
+        throw `Unknown gametype: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -43,7 +51,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
